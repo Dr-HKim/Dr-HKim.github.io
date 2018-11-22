b8eac21478bb192994fc3c8f8d68e6d2da8f138f
HKim <yui7890@gmail.com>
2018-11-22 22:05:40 +0900

    update

--- RMD_to_MD.R
+++ RMD_to_MD.R
@@ -14,8 +14,9 @@ rmds <- "_Rmd"
 
 ## 파일 이름 지정
 FILELIST <- list.files(path= "./_Rmd", pattern = ".*.Rmd") # subdirectory 에 있는 *.Rmd 파일 리스트 작성
+CONV_FILELIST <- FILELIST[71:72] # 한꺼번에 모두 하면 시간이 오래 걸리므로 변환할 파일을 따로 지정
 
-for (filename in FILELIST){
+for (filename in CONV_FILELIST){
   ## 폴더 경로들
   figs.path <- "assets/article_images/" # 그림이 저장될 경로
   posts.path <- "_posts/Rmd/"           # 변환된 md 파일이 저장될 경로
@@ -39,6 +40,8 @@ for (filename in FILELIST){
 }
 
 ## Rmd 를 MD 로 변환한 파일에서 이미지 링크를 수정
+## base 패키지에 있는 readLines() writeLines() 함수를 사용하면 한글이 깨지므로
+## readr 패키지에 있는 read_lines() 와 write_lines() 함수를 사용 
 MD_FILELIST <- list.files(path= "./_posts/Rmd", pattern = ".*.md") # subdirectory 에 있는 *.md 파일 리스트 작성
 
 for (md_filename in MD_FILELIST) {
@@ -53,6 +56,23 @@ for (md_filename in MD_FILELIST) {
   write_lines(lines_md_filename, file)
 }
 
+
+
+
+# ## Rmd 파일을 읽고 ```r 을 ```{r} 로 변경 
+# Rmd_FILELIST <- list.files(path= "./_Rmd", pattern = ".*.Rmd") # subdirectory 에 있는 *.Rmd 파일 리스트 작성
+# 
+# for (Rmd_filename in Rmd_FILELIST) {
+#   file <- paste0("_Rmd/", Rmd_filename)
+#   lines_Rmd_filename <- read_lines(file)
+#   
+#   for (i in 1:length(lines_Rmd_filename)){
+#     if (startsWith(lines_Rmd_filename[i],"```r")) {
+#       lines_Rmd_filename[i] <- sub("```r", "```{r}", lines_Rmd_filename[i])
+#     }
+#   }
+#   write_lines(lines_Rmd_filename, file)
+# }
 
 ## Rmd 파일 이름을 수정할 때를 대비하여, 기존의 md 파일을 모두 지우는 코드 필요
 
---
+++ _Rmd/2017-12-10-Vector-Autoregression.Rmd
@@ -0,0 +1,18 @@
+---
+layout: post  
+title: 벡터자기회귀모형 (Vector Autoregression)  
+date: 2017-12-09  
+category: [Data Analysis]  
+tag: [R]  
+author: hkim  
+hidden: true # don't count this post in blog pagination  
+image: /assets/images/icon/iconmonstr-monitoring-6-240.png  
+headerImage: true  
+
+---
+
+***preface*** 이번 포스트에서는 경제 변수를 분석할 때 널리 사용되는 벡터자기회귀모형(Vector Autoregression, VAR)에 대하여 알아봅니다.
+
+다음 자료를 참고하였습니다:  
+- [An Introduction to Vector Autoregression (VAR)](https://econometricswithr.wordpress.com/time-series/an-introduction-to-vector-autoregression-var/)
+- [A brief introduction to the vector autoregression](http://rstudio-pubs-static.s3.amazonaws.com/118233_f0fc946f71074d96933238c3628358c4.html)
